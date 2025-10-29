adc48cc7a3e33064663efb5a72abb76bfae37ece
hziulq <kd1295154@st.kobedenshi.ac.jp>
2025-10-29 22:42:42 +0900

    created apidb

---
+++ apidb/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
