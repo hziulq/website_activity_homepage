30e4d33452bb29bca5b67a11a37a9ea17925f1b2
hziulq <kd1295154@st.kobedenshi.ac.jp>
2025-11-05 23:23:09 +0900

    .env.exapmle added

--- apidb/index.js
+++ apidb/index.js
@@ -8,10 +8,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 20, // プールの最大接続数
+  idleTimeoutMillis: 30000, // 接続がアイドル状態で破棄されるまでの時間
+});
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/news', async (req, res) => {
+app.get('/news', async (req, res) => {
     try {
         
     }
@@ -20,6 +30,8 @@ app.post('/news', async (req, res) => {
         console.log(err.message);
     }
 })
+
+app.get("/update")
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
